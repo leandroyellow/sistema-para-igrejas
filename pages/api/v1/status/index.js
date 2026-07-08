be14c9bf71a67ecdd09c63990c4a93883ef7d83d
leandro notebook <leandroqueiroztrepador@gmail.com>
2026-07-07 22:48:24 -0300

    add `.env` variaveis de ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,7 @@ import database from "../../../../infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
+  console.log(result.rows);
   response.status(200).json({ chave: "sou aluno acima da média" });
 }
 export default status;
