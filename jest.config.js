24a20754266f8ed1c8def763a54654e34b67e1dc
leandro notebook <leandroqueiroztrepador@gmail.com>
2026-07-14 19:08:44 -0300

    cria npm test com orchestrator

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ const createJestConfig = nextJest({
 });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 6000,
 });
 module.exports = jestConfig;
