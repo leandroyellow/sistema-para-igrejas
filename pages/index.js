b3fceda85bb5ebb2d8b10357d4d08fa90c92a541
leandro notebook <leandroqueiroztrepador@gmail.com>
2026-07-14 13:23:16 -0300

    add nova branch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Página em construção de um sistema para ser aplicado em igrejas, e
-      abençoar vidas!
+      abençoar vidas!teste
     </h1>
   );
 }
